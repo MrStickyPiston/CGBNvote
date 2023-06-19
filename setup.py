10823531f039c76ebfe0df6b141b4fc6dedb2947
sticky <mr.sticky.piston@gmail.com>
2023-06-19 13:57:32 +0200

    User friendly gui based setup

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
-import subprocess
 import json
-import sys
-import os
-import math
 import secrets
+import subprocess
+import sys
 
-
-def header(message, char="=", charamount=-1, whitespace=0):
-    if charamount == -1:
-        charamount = int(math.ceil((100 - len(message)) / 2))
-    return "\n" * whitespace + charamount * char + " " + message + " " + charamount * char
+import lib
 
 
 def install_packages():
-    print(header("pip", char="~"))
     try:
         import pip
         print("Pip has been located.\nNot installing pip.")
@@ -22,50 +15,39 @@ def install_packages():
         subprocess.check_call([sys.executable, 'setup/get-pip.py'])
         print("installed pip")
 
-    print(header("installing packages", char="~"))
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'bottle', 'matplotlib', 'gunicorn', 'waitress'])
-
-
-def generate_config():
-    config = eval(open("setup/templates/project_config.pyjson").read(), {'input': input, '__builtins__': None, 'secrets': secrets})
-    json_config = json.dumps(config, indent=4)
-
-    with open("config.json", "w") as outfile:
-        outfile.write(json_config)
 
 
 def generate_database():
     def check_password(password):
         SpecialSym = """!"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"""
         valid = True
+        problems = ""
 
         if len(password) < 6:
-            print('Passsword length should be at least 6')
+            problems += ' Passsword length should be at least 6,'
             valid = False
 
         if not any(char.isdigit() for char in password):
-            print('Password should have at least one numeral')
+            problems += ' Password should have at least one numeral,'
             valid = False
 
         if not any(char.isupper() for char in password):
-            print('Password should have at least one uppercase letter')
+            problems += ' Password should have at least one uppercase letter,'
             valid = False
 
         if not any(char.islower() for char in password):
-            print('Password should have at least one lowercase letter')
+            problems += ' Password should have at least one lowercase letter,'
             valid = False
 
         if not any(char in SpecialSym for char in password):
-            print('Password should have at least one special character')
+            problems += ' Password should have at least one special character,'
             valid = False
-        if valid:
-            return valid
 
-    def get_password():
-        password = input("Enter a admin password: ")
-        while not check_password(password):
-            password = input("Enter a strong admin password: ")
-        return password
+        if valid:
+            return [valid]
+        else:
+            return [valid, problems[:-1]]
 
     con = lib.database.connect("database.db")
     lib.database.setup(con)
@@ -75,57 +57,56 @@ def generate_database():
 
     lib.database.set_candidates(con, candidates)
 
-    lib.database.set_admins(con, [(input("Enter admin username: "), get_password())])
+    password = bottle.request.forms["password"]
+    validation = check_password(password)
+    if not validation[0]:
+        return bottle.template("script", {"script": f"alert('Incorrect password: {validation[1]}'); history.back()"})
+
+    lib.database.set_admins(con, [(bottle.request.forms["user"], password)])
     lib.database.set_settings(con, db_config)
-    print(
-        "NOTE: Voting is disabled now, but you can enable it on the admin panel. The candidates of 2023 are already set, but if you need other candidates you can set them there. Also dont forget to remove the disclaimer.")
     con.close()
 
 
+def generate_config():
+    config = eval(open("setup/templates/project_config.pyjson").read(), {'bottle': bottle,
+                                                                         '__builtins__': None,
+                                                                         'secrets': secrets})
+    json_config = json.dumps(config, indent=4)
+
+    with open("config.json", "w") as outfile:
+        outfile.write(json_config)
+
+
 if __name__ == "__main__":
-    print(header("PYTHON PACKAGES"))
     install_packages()
 
-    print(header("CONFIGURATION"))
-    if not os.path.exists(os.getcwd() + "/config.json"):
-        # project configuration for new config
-        print(header("CGBNvote instance configuration", char="~"))
-        generate_config()
+import bottle
 
-    elif not input("An existing config was found. Do you want to overwrite it? (y/n): ") == "y":
-        print("Skipping config")
-        pass
 
-    else:
-        # project configuration for existing config
-        print(header("CGBNvote instance configuration", char="~"))
-        os.remove(os.getcwd() + "/config.json")
-        generate_config()
+@bottle.get('/')
+def setup_form():
+    return bottle.template("setup_form")
 
-    # Check the database exists for later so that lib.web won't throw a mail exception.
-    db_exists = os.path.exists(os.getcwd() + "/database.db")
-    import lib
 
-    if not db_exists:
-        print(header("admin configuration", char="~"))
-        generate_database()
-    elif not input("\nAn existing database was found. Do you want to overwrite it? (y/n): ") == "y":
-        print("Skipping database")
-        pass
-    else:
-        print(header("admin configuration", char="~"))
-        os.remove(os.getcwd() + "/database.db")
-        generate_database()
-    print("")
+@bottle.post('/')
+def setup_form():
+    config_response = generate_config()
+    database_response = generate_database()
 
-    print(header("static files folder", char="~"))
+    if config_response is not None:
+        return config_response
+    elif database_response is not None:
+        return database_response
+
     try:
         os.mkdir(os.getcwd() + "/static/")
         print("Created folder /static/")
     except FileExistsError:
         print("ERROR: folder /static/ already exists")
 
-    print("\nInstallation done. Check README.md for further information.")
+    subprocess.Popen([sys.executable, "server.py"])
 
-    if input("Do you want to start the server? (y/n): ") == "y":
-        subprocess.check_call([sys.executable, "server.py"])
+    return bottle.template("custom", {"content": "NOTE: Voting is disabled now, but you can enable it on the <a href='/admin-panel'>admin panel</a>. The candidates of 2023 are already set, but if you need other candidates you can set them there. Also dont forget to remove the disclaimer."})
+
+
+bottle.run()
---
+++ views/setup_form.tpl
@@ -0,0 +1,225 @@
+<body id="i5qz">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
+  <title>CGBNvote setup
+  </title>
+  <header id="i7wq-2" class="header-banner">
+  </header>
+  <section id="iq09n-2" class="flex-sect">
+    <div id="iq7l-2" class="container-width">
+      <div id="ibn2g-2" class="flex-title">CGBNvote setup
+        <br/>
+      </div>
+    </div>
+    <div id="iy8d-2" class="cards">
+      <form method="post" id="itxx7" action="/">
+        <fieldset id="ih2z2">
+          <legend id="i9w0u">Server url</legend>
+          <input type="text" id="user" name="server_url" required/>
+        </fieldset>
+        <fieldset id="iwdsq">
+          <legend id="ikmaf">Server email</legend>
+          <input type="text" name="mail" required id="in2qs" placeholder=""/>
+        </fieldset>
+        <fieldset id="ilxwo">
+          <legend id="iqg7q">Server email wachtwoord</legend>
+          <input type="password" name="mail_password" required id="iwzni"/>
+        </fieldset>
+        <fieldset id="iw1w9">
+          <legend id="ifmvi">Admin gebruikersnaam</legend>
+          <input type="text" name="user" required id="iapcm"/>
+        </fieldset>
+        <fieldset id="ispc8">
+          <legend id="i0b1u">Admin wachtwoord</legend>
+          <input type="password" id="imzij" name="password" required/>
+        </fieldset>
+        <button type="submit" id="ifdlg">Installatie voltooien</button>
+        <div id="i2qefk">
+        </div>
+        <div id="ibo35">
+        </div>
+      </form>
+    </div>
+  </section>
+</body>
+<style>* {
+  box-sizing: border-box;
+  }
+  body {
+    margin: 0;
+  }
+  *{
+    box-sizing:border-box;
+  }
+  body{
+    margin:0;
+  }
+  .header-banner{
+    padding-top:35px;
+    padding-bottom:100px;
+    color:#ffffff;
+    font-family:Helvetica, serif;
+    font-weight:100;
+    background-image:url("https://grapesjs.com/img/bg-gr-v.png"), url("https://grapesjs.com/img/work-desk.jpg");
+    background-attachment:scroll, scroll;
+    background-position:left top, center center;
+    background-repeat:repeat-y, no-repeat;
+    background-size:contain, cover;
+  }
+  .container-width{
+    width:90%;
+    max-width:1150px;
+    margin:0 auto;
+  }
+  .flex-sect{
+    background-color:#fafafa;
+    padding:50px 0;
+    font-family:Helvetica, serif;
+  }
+  .flex-title{
+    margin-bottom:15px;
+    font-size:2em;
+    text-align:center;
+    font-weight:700;
+    color:#555;
+    padding:5px;
+  }
+  .cards{
+    padding:20px 0;
+    display:flex;
+    justify-content:space-around;
+    flex-flow:wrap;
+  }
+  #itxx7{
+    display:flex;
+    flex-direction:column;
+    background-repeat:repeat;
+    background-position:left top;
+    background-attachment:scroll;
+    background-size:auto;
+    background-image:linear-gradient(to right, #bf387e 0%, #cd5e5e 100%);
+    padding:10px 10px 10px 10px;
+    box-shadow:0 0 5px 0 black;
+    opacity:0.86;
+    border-radius:5px 5px 5px 5px;
+    margin:0 10 0 10;
+    max-width:400px;
+    min-width:300px;
+    width:25%;
+  }
+  #user{
+    display:flex;
+    margin:0 0 6px 0;
+    width:100%;
+    height:30px;
+    border-radius:5px 5px 5px 5px;
+    border:0px solid white;
+  }
+  #imzij{
+    margin:0 0 6px 0;
+    display:flex;
+    flex-direction:row;
+    justify-content:flex-start;
+    align-items:flex-start;
+    width:100%;
+    height:30px;
+    border:0px solid white;
+    border-radius:5px 5px 5px 5px;
+  }
+  #i7wq-2{
+    background-repeat:repeat-y, no-repeat;
+    background-position:left top, center center;
+    background-attachment:scroll, scroll;
+    background-size:contain, cover;
+    background-image:url("https://grapesjs.com/img/bg-gr-v.png"), url('https://www.gemeente.nu/content/uploads/sites/5/2017/02/stemmen.jpg');
+  }
+  #ispc8{
+    margin:0px 0px 12px 0px;
+    display:flex;
+  }
+  #i9w0u{
+    color:#ffffff;
+  }
+  #i0b1u{
+    color:#ffffff;
+  }
+  #ifdlg{
+    border-radius:5px 5px 5px 5px;
+    opacity:1;
+    border:0px solid white;
+    padding:0px 2.5px 0px 2.5px;
+    height:30px;
+    background-repeat:repeat;
+    background-position:left top;
+    background-attachment:scroll;
+    background-size:auto;
+    background-image:linear-gradient(#ffffff 0%, #ffffff 100%);
+    width:100%;
+    margin:0 10px 0 0;
+    display:block;
+  }
+  #ih2z2{
+    margin:0 0 6px 0;
+  }
+  #ifdlg:active{
+    box-shadow:0 0 5px 0 black;
+  }
+  input::-webkit-outer-spin-button, input::-webkit-inner-spin-button{
+    -webkit-appearance:none;
+    margin:0;
+  }
+  input[type=number]{
+    -moz-appearance:textfield;
+  }
+  #ifmvi{
+    color:#ffffff;
+  }
+  #iapcm{
+    display:flex;
+    margin:0 0 6px 0;
+    width:100%;
+    height:30px;
+    border-radius:5px 5px 5px 5px;
+    border:0px solid white;
+  }
+  #iw1w9{
+    margin:0 0 6px 0;
+  }
+  #iqg7q{
+    color:#ffffff;
+  }
+  #iwzni{
+    display:flex;
+    margin:0 0 6px 0;
+    width:100%;
+    height:30px;
+    border-radius:5px 5px 5px 5px;
+    border:0px solid white;
+  }
+  #ilxwo{
+    margin:0 0 6px 0;
+  }
+  #ikmaf{
+    color:#ffffff;
+  }
+  #in2qs{
+    display:flex;
+    margin:0 0 6px 0;
+    width:100%;
+    height:30px;
+    border-radius:5px 5px 5px 5px;
+    border:0px solid white;
+  }
+  #iwdsq{
+    margin:0 0 6px 0;
+  }
+  @media (max-width: 992px){
+    #itxx7{
+      width:50%;
+    }
+  }
+  @media (max-width: 480px){
+    #itxx7{
+      width:80%;
+    }
+  }
+</style>
